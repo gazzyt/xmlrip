f4ff3a81a06e6bc4f4d81232459491c3329c5568
Gary Thomas <gazzyt@live.co.uk>
2017-12-22 14:49:02 +0000

    Can now iterate a LibXmlAttributeCollection

--- libxmlrip/libxmlattributecollection.h
+++ libxmlrip/libxmlattributecollection.h
@@ -3,12 +3,27 @@
 
 #include "libxml/xmlstring.h"
 
+#include "libxmlattributecollection-iterator.h"
+
 
 class LibXmlAttributeCollection
 {
 public:
+	typedef LibXmlAttributeCollection_iterator const_iterator;
+
+public:
 	LibXmlAttributeCollection(const xmlChar** attributes) : m_attributes(attributes)
 	{};
+
+	const_iterator begin() const
+	{
+		return LibXmlAttributeCollection_iterator{ m_attributes };
+	}
+
+	const_iterator end() const
+	{
+		return LibXmlAttributeCollection_iterator{};
+	}
 
 
 private:
--- testxmlrip/libxmlattributecollection-test.cpp
+++ testxmlrip/libxmlattributecollection-test.cpp
@@ -11,11 +11,32 @@ using namespace std;
 
 static const xmlChar* testStrings[] = { BAD_CAST "attname1", BAD_CAST "attvalue1", BAD_CAST "attname2", BAD_CAST "attvalue2" };
 
-TEST(LibXmlAttributeCollection, Test1) {
+TEST(LibXmlAttributeCollection, MemberBeginIteratesTwoTimes) {
 	// Arrange
 	LibXmlAttributeCollection coll(testStrings);
+	unsigned count = 0;
 
 	//Act
+	for (auto iter = coll.begin(); iter != coll.end(); ++iter)
+	{
+		++count;
+	}
+
 	// Assert
+	EXPECT_EQ(2, count);
 }
 
+TEST(LibXmlAttributeCollection, GlobalBeginIteratesTwoTimes) {
+	// Arrange
+	LibXmlAttributeCollection coll(testStrings);
+	unsigned count = 0;
+
+	//Act
+	for (auto iter = begin(coll); iter != end(coll); ++iter)
+	{
+		++count;
+	}
+
+	// Assert
+	EXPECT_EQ(2, count);
+}
